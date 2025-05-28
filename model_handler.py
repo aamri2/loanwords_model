9b9c8a46f92b5fc8983fe2e7fee6b07470b128c6
aamri2 <hassan-khan@live.ca>
2025-05-28 13:12:00 -0400

    added one-step probability calculating

--- model_handler.py
+++ model_handler.py
@@ -1,15 +1,24 @@
 import pandas, numpy
 import torch
 import json
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
-from pyctcdecode import build_ctcdecoder
+from transformers import Wav2Vec2Config, AutoModel, PreTrainedModel, Wav2Vec2FeatureExtractor
+from train_sequence_classification import Wav2Vec2WithAttentionClassifier
+from datasets import Dataset
+from pyctcdecode.decoder import build_ctcdecoder
+from typing import Union, Self
 
 model_dir = '../models'
-model = 'final_model_folded'
+model_name = 'final_model_classification'
 
-with open(f'{model_dir}/{model}/vocab.json') as f:
-    vocab_dict = json.load(f)
-vocab_list = [x[0] for x in sorted(vocab_dict.items(), key = lambda x: x[1])]
+model = Wav2Vec2WithAttentionClassifier.from_pretrained(f'{model_dir}/{model_name}')
+feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(f'{model_dir}/{model_name}')
+
+# with open(f'{model_dir}/{model_name}/vocab.json') as f:
+#     vocab_dict = json.load(f)
+# vocab_list = [x[0] for x in sorted(vocab_dict.items(), key = lambda x: x[1])]
+
+
+
 
 def map_to_result(batch):
     with torch.no_grad():
@@ -30,14 +39,17 @@ def map_to_result_no_labels(batch):
     batch['logits'] = logits
     return batch
 
-processor = Wav2Vec2Processor.from_pretrained(f'{model_dir}/{model}')
-model = Wav2Vec2ForCTC.from_pretrained(f'{model_dir}/{model}')
-decoder = build_ctcdecoder([chr(i) for i in range(63)]) # unique two-character sequences
+# processor = Wav2Vec2Processor.from_pretrained(f'{model_dir}/{model}')
+# model = Wav2Vec2ForCTC.from_pretrained(f'{model_dir}/{model}')
+# decoder = build_ctcdecoder([chr(i) for i in range(63)]) # unique two-character sequences
+
+
 
 def centre_probabilities(batch):
+    """Used for CTC pooling"""
     audio = batch['audio']
     with torch.no_grad():
-        input_values = torch.tensor(processor(audio['array'], sampling_rate=audio['sampling_rate']).input_values[0]).unsqueeze(0)
+        input_values = torch.tensor(feature_extractor(audio['array'], sampling_rate=audio['sampling_rate']).input_values[0]).unsqueeze(0)
         logits = model(input_values).logits.cpu().detach()[0]
     
     centre = len(logits) // 2
@@ -55,14 +67,14 @@ def count_where(df, **column_values):
     
 def probability(probabilities, responses, response = None, **column_values):
     index = [i for i in range(len(probabilities)) if all(probabilities[column][i] == value for column, value in column_values.items())]
-    mean_probabilities = numpy.array(probabilities[index]['probabilities']).mean(0)[[vocab_list.index(i) for i in responses]]
+    mean_probabilities = numpy.array(probabilities[index]['probabilities']).mean(0)[[range(model.config.num_labels)]]
     normed_probabilities = mean_probabilities * 1/mean_probabilities.sum()
     if response:
         return normed_probabilities[responses.index(response)]
     else:
         return normed_probabilities
 
-def pool(probabilities, responses, index, index_names):
+def pool(probabilities, responses: Union[list, dict], index, index_names):
     if isinstance(responses, list):
         responses = {response: response for response in responses}
         
@@ -70,7 +82,7 @@ def pool(probabilities, responses, index, index_names):
         index = [(i,) for i in index]
         index_names = [index_names]
 
-    pooled_probabilities = pandas.DataFrame(columns = responses.keys(), index = ['.'.join(indices) for indices in index])
+    pooled_probabilities = pandas.DataFrame(columns = model.config.id2label.values(), index = ['.'.join(indices) for indices in index])
     for indices in index:
         pooled_probabilities.loc['.'.join(indices)] = probability(probabilities, [value for key, value in responses.items()], **{index_name: i for i, index_name in zip(indices, index_names)})
     
@@ -78,3 +90,29 @@ def pool(probabilities, responses, index, index_names):
         pooled_probabilities[column] = [float(i) for i in pooled_probabilities[column]]
     
     return pooled_probabilities
+
+
+def probabilities(model, dataset: Dataset, id2label = None):
+    """Create DataFrame in long format containing classification probabilities"""
+
+    column_names = [column_name for column_name in dataset.column_names if column_name != 'input_values']
+
+    data = {
+        'probabilities': [],
+        'classification': [],
+        **{column_name: [] for column_name in column_names}
+    }
+    if not id2label:
+        id2label = model.config.id2label if model.config.id2label else {}
+    
+    for row in dataset.to_iterable_dataset():
+        with torch.no_grad():
+            input_values = torch.tensor(row['input_values'])#.unsqueeze(0)
+            logits = model(input_values)['logits'].cpu().detach()[0]
+        probabilities = torch.nn.functional.softmax(logits, dim=-1)
+        data['probabilities'].extend(probabilities)
+        data['classification'].extend(id2label.values())
+        for column_name in column_names:
+            data[column_name].extend([row[column_name]] * len(probabilities))
+    
+    return pandas.DataFrame(data)
