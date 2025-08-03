4df653232df7a9a0ae14ea16ddeda153b79f5db2
aamri2 <hassan-khan@live.ca>
2025-08-03 02:09:01 -0400

    noisy and substrings keep originals functionality

--- dataset_handler.py
+++ dataset_handler.py
@@ -98,13 +98,13 @@ def random_substrings(aligned_dataset, samples_to_frames_ratio, substring_length
     aligned_dataset = aligned_dataset.map(get_substrings, remove_columns = ['start', 'end'])
     return aligned_dataset
 
-def noisy_and_substrings(aligned_dataset, snr, samples_to_frames_ratio, substring_length = None, substring_ratio = 0.3, noisy_ratio = 0.5, substring_subset_ratio = 0.5):
+def noisy_and_substrings(aligned_dataset, snr, samples_to_frames_ratio, substring_length = None, substring_ratio = 0.3, noisy_ratio = 0.5, substring_subset_ratio = 0.5, keep_originals = False):
     """Helper function makes noisy and substring split of aligned dataset."""
 
     if isinstance(aligned_dataset, DatasetDict):
         new_dataset = DatasetDict()
         for key, dataset in aligned_dataset.items():
-            new_dataset[key] = noisy_and_substrings(dataset, snr, samples_to_frames_ratio, substring_length, substring_ratio, noisy_ratio, substring_subset_ratio)
+            new_dataset[key] = noisy_and_substrings(dataset, snr, samples_to_frames_ratio, substring_length, substring_ratio, noisy_ratio, substring_subset_ratio, keep_originals)
         return new_dataset
     
     random_indices = np.random.choice(range(len(aligned_dataset)), size=(len(aligned_dataset,)), replace=False)
@@ -116,7 +116,10 @@ def noisy_and_substrings(aligned_dataset, snr, samples_to_frames_ratio, substrin
         )
     else:
         noisy_dataset = Dataset.from_dict({'input_values': [], 'labels': [], 'start': [], 'end': []})
-    clean_dataset = aligned_dataset.select(random_indices[int(noisy_ratio*len(aligned_dataset)):])
+    if keep_originals:
+        clean_dataset = aligned_dataset
+    else:
+        clean_dataset = aligned_dataset.select(random_indices[int(noisy_ratio*len(aligned_dataset)):])
 
     # make substring subsets
     if substring_subset_ratio > 0:
@@ -129,8 +132,12 @@ def noisy_and_substrings(aligned_dataset, snr, samples_to_frames_ratio, substrin
     else:
         noisy_substring_dataset = Dataset.from_dict({'input_values': [], 'labels': []})
         clean_substring_dataset = Dataset.from_dict({'input_values': [], 'labels': []})
-    noisy_string_dataset = noisy_dataset.select(range(int(substring_subset_ratio*len(noisy_dataset)), len(noisy_dataset))).remove_columns(['start', 'end'])
-    clean_string_dataset = clean_dataset.select(range(int(substring_subset_ratio*len(clean_dataset)), len(clean_dataset))).remove_columns(['start', 'end'])
+    if keep_originals:
+        noisy_string_dataset = noisy_dataset.remove_columns(['start', 'end'])
+        clean_string_dataset = clean_dataset.remove_columns(['start', 'end'])
+    else:
+        noisy_string_dataset = noisy_dataset.select(range(int(substring_subset_ratio*len(noisy_dataset)), len(noisy_dataset))).remove_columns(['start', 'end'])
+        clean_string_dataset = clean_dataset.select(range(int(substring_subset_ratio*len(clean_dataset)), len(clean_dataset))).remove_columns(['start', 'end'])
 
     new_dataset = datasets.concatenate_datasets([noisy_substring_dataset, noisy_string_dataset, clean_substring_dataset, clean_string_dataset])
     new_dataset = new_dataset.shuffle().flatten_indices()
