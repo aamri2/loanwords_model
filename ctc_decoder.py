9c09e32621ddfb088a4a6c5979b9b8913a6779e1
aamri2 <hassan-khan@live.ca>
2025-08-13 18:06:50 -0400

    typing and small changes for consistency

--- ctc_decoder.py
+++ ctc_decoder.py
@@ -1,9 +1,26 @@
 import torch
 import k2
 from collections import defaultdict
-from typing import Optional
+from typing import Optional, Union, TypeVar, Literal, NamedTuple, Sequence, overload
 
-def decode_probabilities(symbols: list, symbol_of_interest: int, logits: torch.Tensor, vocab: Optional[dict] = None, pad_token_id = 0, as_strings = False) -> torch.Tensor:
+T = TypeVar('T', int, str)
+Recursive = Union[T, Sequence['Recursive[T]']]
+RecursiveTuple = Union[T, tuple['RecursiveTuple[T]']]
+Symbols = Sequence[Recursive[int]]
+StringSymbols = Sequence[Recursive[str]]
+AnySymbols = Union[Symbols, StringSymbols]
+
+class Probabilities(NamedTuple):
+    probabilities: torch.Tensor
+    classifications: AnySymbols
+
+@overload
+def decode_probabilities(symbols: Symbols, symbol_of_interest: int, logits: torch.Tensor, vocab: None, pad_token_id: int, as_strings: Literal[False]) -> Probabilities:
+    pass
+@overload
+def decode_probabilities(symbols: StringSymbols, symbol_of_interest: int, logits: torch.Tensor, vocab: dict[str, Recursive[int]], pad_token_id: int, as_strings: Literal[True]) -> Probabilities:
+    pass
+def decode_probabilities(symbols: AnySymbols, symbol_of_interest: int, logits: torch.Tensor, vocab: Optional[dict[str, Recursive[int]]] = None, pad_token_id: int = 0, as_strings: bool = False) -> Probabilities:
     """
     Given some possible decodings, returns the relative probabilities of each.
 
@@ -34,29 +51,73 @@ def decode_probabilities(symbols: list, symbol_of_interest: int, logits: torch.T
             raise ValueError('vocab required when as_strings True!')
         symbols = _str_to_symbols(symbols, vocab)
     
+    if not isinstance(symbols[symbol_of_interest], list) or not len(symbols[symbol_of_interest]) > 1:
+        raise ValueError('Symbol of interest must have multiple options!')
+
     if not pad_token_id == 0:
         symbols = _zero_pad_token_id(symbols, pad_token_id)
         logits = _zero_pad_token_id_logits(logits, pad_token_id)
     
     decodings = _get_decodings(symbols, logits)
-    symbols_lists = _get_symbols_lists(symbols, symbol_of_interest)
+    symbols_lists = get_symbols_lists(symbols, symbol_of_interest)
     scores = _get_scores(symbols_lists, decodings)
-    probabilities = scores.softmax(0)
+    probabilities = scores.softmax(1)
+    classifications = [symbols_list[symbol_of_interest] for symbols_list in symbols_lists]
+    
+    if not pad_token_id == 0:
+        classifications = _unzero_pad_token_id(classifications, pad_token_id)
 
-    return probabilities
+    if as_strings:
+        classifications = _symbols_to_str(classifications, vocab)
 
+    return Probabilities(probabilities, classifications)
 
+def get_symbols_lists(symbols: AnySymbols, symbol_of_interest: int) -> list[Symbols]:
+    """
+    Convenience function to easily distinguish paths of interest.
+
+    For example, if you want to allow for decodings that begin with any consonant
+    `['p', 't']` and end with `'b'`, but you are specifically interested in
+    whether the medial vowel is either `'i'` or `'u'`., you would enter
+    `symbols = [['p', 't'], ['i', 'u'], 'b']` and `symbol_of_interest = 1`.
+    This would return the list of symbols lists of interest
+    `[[['p', 't'], 'i', 'b'], [['p', 't'], 'u', 'b']]` as required by, e.g.,
+    `_get_tot_scores`. This way, you don't have to separately write these out.
+
+    The function is agnostic to whether these are symbols or characters,
+    and to whether the symbol of interest is a symbol or a list of symbols.
+    This means that something like `symbols = ['t', ['u', ['j', 'u']], 'n']`
+    with `symbol_of_interest = 1`, comparing the decodings 'tun' and 'tjun',
+    is permitted.
+    """
+
+    if symbol_of_interest < 0 or symbol_of_interest > len(symbols):
+        raise ValueError(f'Symbol of interest {symbol_of_interest} not in range!')
+    
+    symbols_of_interest = symbols[symbol_of_interest]
+    if not isinstance(symbols_of_interest, list):
+        raise ValueError(f'Symbol of interest {symbol_of_interest} not a list!')
+
+    symbols_lists = [symbols[:symbol_of_interest] + [symbol] + symbols[symbol_of_interest + 1:] for symbol in symbols_of_interest]
+    return symbols_lists
 
 class Nodes(dict):
     def __missing__(self, key): # newest node should have highest number
         self[key] = len(self)
         return self[key]
 
-def _str_to_symbols(string: list[str], vocab: dict):
+def _str_to_symbols(string: StringSymbols, vocab: dict[str, Recursive[int]]) -> Symbols:
     """Translates strings to symbols using a vocabulary. Multiple paths are permitted."""
+
     return [vocab[char] if isinstance(char, str) else _str_to_symbols(char, vocab) for char in string]
 
-def _get_paths(symbols: list, prev_symbol = 0, nodes: Optional[Nodes] = None, edges: Optional[defaultdict] = None, position = 1) -> tuple[Nodes, defaultdict]:
+def _symbols_to_str(symbols: Symbols, vocab: dict[str, Recursive[int]]) -> StringSymbols:
+    """Translates symbols to strings using a vocabulary. Multiple paths are permitted."""
+
+    reversed_vocab = {value: key for key, value in vocab.items() if isinstance(value, int)}
+    return [reversed_vocab[symbol] if isinstance(symbol, int) else _symbols_to_str(symbol, vocab) for symbol in symbols]
+
+def _get_paths(symbols: Symbols, prev_symbol = 0, nodes: Optional[Nodes] = None, edges: Optional[defaultdict] = None, position = 1) -> tuple[Nodes, defaultdict]:
     """
     Recursive paths from previous symbol to remaining symbols as a list of nodes and edges.
     
@@ -113,20 +174,23 @@ def _paths_to_fst(paths: tuple[Nodes, defaultdict]) -> k2.Fsa:
     fst_str = '\n'.join(sorted(fst_str, key = lambda x: int(x.split()[0])) + [str(len(nodes))])
     return k2.arc_sort(k2.Fsa.from_str(fst_str, acceptor = False)) # type: ignore # ret_arc_map is False
 
-def _get_decodings(symbols: list, logits: torch.Tensor) -> k2.Fsa:
+def _get_decodings(symbols: Symbols, logits: torch.Tensor) -> k2.Fsa:
     """Given a list of symbols and logits, returns the lattice of CTC decodings."""
 
     a_fst = k2.ctc_topo(logits.shape[2]) # vocabulary size is size of dimension 2
     b_fst = _paths_to_fst(_get_paths(symbols))
 
     ab_fst = k2.connect(k2.compose(a_fst, b_fst)) # graph of all possible decodings
-    decodings = k2.get_lattice(logits, torch.tensor([logits.shape[1]]), ab_fst, search_beam = 100, output_beam = 100, min_active_states = 0)
+    decodings = k2.get_lattice(logits, torch.full_like(logits[:, 0, 0], logits.shape[1]), ab_fst, search_beam = 100, output_beam = 100, min_active_states = 0)
 
     return decodings
 
-def _get_scores(symbols_lists: list[list], decodings: k2.Fsa) -> torch.Tensor:
+def _get_scores(symbols_lists: list[Symbols], decodings: k2.Fsa) -> torch.Tensor:
     """
     Gives scores for different possible decodings from a list of mutually exclusive decodings.
+
+    Returns a tensor of shape (N, C), where N is the batch size and C is the number
+    of lists to compare.
     
     Each item in `symbols_lists` should be a full, possible, and unique set of possible decodings
     to compose with the decodings.
@@ -140,45 +204,17 @@ def _get_scores(symbols_lists: list[list], decodings: k2.Fsa) -> torch.Tensor:
 
     if any(symbols_lists.count(x) > 1 for x in symbols_lists): # non-unique
         raise ValueError(f'Symbols lists {symbols_lists} contains duplicate decodings.')
-    scores = torch.empty(len(symbols_lists))
+    scores = torch.empty(decodings.shape[0], len(symbols_lists))
 
     for i, symbols in enumerate(symbols_lists):
         subset_fst = _paths_to_fst(_get_paths(symbols))
         decodings_subset_fst = k2.connect(k2.compose(decodings, subset_fst))
-        if decodings_subset_fst.shape[0] == 0: # path invalid
+        if decodings_subset_fst[0].shape[0] == 0: # path invalid
             raise ValueError(f'Symbols {symbols} not found in decodings {decodings}.')
-        scores[i] = decodings_subset_fst.get_tot_scores(True, True)
+        with torch.no_grad():
+            scores[:, i] = decodings_subset_fst.get_tot_scores(True, True)
     
     return scores
-
-def _get_symbols_lists(symbols: list, symbol_of_interest: int) -> list[list]:
-    """
-    Convenience function to easily distinguish paths of interest.
-
-    For example, if you want to allow for decodings that begin with any consonant
-    `['p', 't']` and end with `'b'`, but you are specifically interested in
-    whether the medial vowel is either `'i'` or `'u'`., you would enter
-    `symbols = [['p', 't'], ['i', 'u'], 'b']` and `symbol_of_interest = 1`.
-    This would return the list of symbols lists of interest
-    `[[['p', 't'], 'i', 'b'], [['p', 't'], 'u', 'b']]` as required by, e.g.,
-    `_get_tot_scores`. This way, you don't have to separately write these out.
-
-    The function is agnostic to whether these are symbols or characters,
-    and to whether the symbol of interest is a symbol or a list of symbols.
-    This means that something like `symbols = ['t', ['u', ['j', 'u']], 'n']`
-    with `symbol_of_interest = 1`, comparing the decodings 'tun' and 'tjun',
-    is permitted.
-    """
-
-    if symbol_of_interest < 0 or symbol_of_interest > len(symbols):
-        raise ValueError(f'Symbol of interest {symbol_of_interest} not in range!')
-    
-    symbols_of_interest = symbols[symbol_of_interest]
-    if not isinstance(symbols_of_interest, list):
-        raise ValueError(f'Symbol of interest {symbol_of_interest} not a list!')
-
-    symbols_lists = [symbols[:symbol_of_interest] + [symbol] + symbols[symbol_of_interest + 1:] for symbol in symbols_of_interest]
-    return symbols_lists
 
 def _zero_pad_token_id_logits(logits: torch.Tensor, pad_token_id: int) -> torch.Tensor:
     """k2 requires the padding token to be 0; this method allows non-compliant logits to be made compliant."""
@@ -186,7 +222,7 @@ def _zero_pad_token_id_logits(logits: torch.Tensor, pad_token_id: int) -> torch.
     logits = torch.cat([logits[:, :, pad_token_id].unsqueeze(-1), logits[:, :, :pad_token_id], logits[:, :, pad_token_id + 1:]], dim = 2)
     return logits
 
-def _zero_pad_token_id(symbols: list, pad_token_id: int) -> list:
+def _zero_pad_token_id(symbols: Symbols, pad_token_id: int) -> Symbols:
     """Takes a list of symbols and shifts them to the adjusted vocabulary for a non-zero pad_token_id."""
 
     translated_symbols = []
@@ -203,7 +239,7 @@ def _zero_pad_token_id(symbols: list, pad_token_id: int) -> list:
     
     return translated_symbols
 
-def _unzero_pad_token_id(symbols: list, pad_token_id: int) -> list:
+def _unzero_pad_token_id(symbols: Symbols, pad_token_id: int) -> Symbols:
     """Takes a translated list of symbols and unshifts them to their original vocabulary."""
 
     untranslated_symbols = []
