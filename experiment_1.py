2af6643545b3cdc64fa4b695c0773b6c84a403bd
aamri2 <hassan-khan@live.ca>
2026-05-24 11:47:00 -0400

    added mfcc training to exp 1

--- experiment_1.py
+++ experiment_1.py
@@ -4,7 +4,7 @@ from transformers.trainer import Trainer
 from transformers.training_args import TrainingArguments
 import evaluate
 import numpy
-from model_architecture import Wav2Vec2LoanwordsModel, DataCollatorWithPaddingForClassification
+from model_architecture import Wav2Vec2LoanwordsModel, DataCollatorWithPaddingForClassification, MFCCLoanwordsModel, MFCCLoanwordsConfig
 from math import ceil
 import torch
 import os
@@ -27,6 +27,13 @@ task = int(sys.argv[1]) # from slurm array task id
 # [EN neural-max native, EN neural-max nonnative]
 # [FR neural-mean native, FR neural-mean nonnative]
 # [FR neural-max native, FR neural-max nonnative]
+# mfcc:
+# [EN neural-mean native, EN neural-mean nonnative]
+# [EN neural-max native, EN neural-max nonnative]
+# [FR neural-mean native, FR neural-mean nonnative]
+# [FR neural-max native, FR neural-max nonnative]
+
+model_config = {'classifier_head': True, 'classifier_hidden': True, 'classifier_hidden_activation_function': 'relu'}
 
 if (task // 10) % 2 == 0:
     dataset_var = 'Responses10Fold'
@@ -37,30 +44,43 @@ elif (task // 10) % 2 == 1:
 
 if (task // 20) % 2 == 0:
     model_name = 'mean'
-    model_config = {'temporal_pooling': 'mean'}
+    model_config |= {'temporal_pooling': 'mean'}
 elif (task // 20) % 2 == 1:
     model_name = 'max'
-    model_config = {'temporal_pooling': 'max', 'max_pooling_windows': 7}
+    model_config |= {'temporal_pooling': 'max', 'max_pooling_windows': 7}
 
 if (task // 40) % 2 == 0:
-    base_model = 'w2v2-large'
+    native_base_model = 'w2v2-large'
     base_dataset = 'wvEN'
     language = 'EN'
 elif (task // 40) % 2 == 1:
-    base_model = 'w2v2fr-large'
+    native_base_model = 'w2v2fr-large'
     base_dataset = 'wvFR'
     language = 'FR'
 
-if (task // 80) % 3 == 0:
-    base_model_type = 'w2v2-nat' # use earlier specified base_model
-elif (task // 80) % 3 == 1:
-    base_model_type = 'w2v2-ml-10'
-    base_model = 'w2v2ml-large-10'
-elif (task // 80) % 3 == 2:
-    base_model_type = 'w2v2-ml-100'
-    base_model = 'w2v2ml-large-100'
+if (task // 80) % 4 < 3:
+    match (task // 80) % 4:
+        case 0:
+            base_model_type = 'w2v2-nat' # use earlier specified base_model
+            base_model = native_base_model
+        case 1:
+            base_model_type = 'w2v2-ml-10'
+            base_model = 'w2v2ml-large-10'
+        case 2:
+            base_model_type = 'w2v2-ml-100'
+            base_model = 'w2v2ml-large-100'
+    model_config |= {'pretrained_model_name_or_path': f'../{base_model}', 'use_weighted_layer_sum': True}
+    model_init_fn = Wav2Vec2LoanwordsModel.from_pretrained
+elif (task // 80) % 4 == 3:
+    base_model_type = 'mfcc'
+    base_model = 'mfcc'
+    model_init_fn = lambda *args, **kwargs: MFCCLoanwordsModel(MFCCLoanwordsConfig(*args, **kwargs))
 
 dataset = datasets.load_from_disk(f'../prep_{base_dataset}{dataset_var}')
+model_config |= {
+    'id2label': {i: v for i, v in enumerate(dataset['train_0'].features['label'].names)},
+    'label2id': {v: i for i, v in enumerate(dataset['train_0'].features['label'].names)},
+}
 fold = task % 10
 os.environ['TENSORBOARD_LOGGING_DIR'] = os.path.expanduser(f'~/scratch/experiment_1_tensorboard/{language}/{base_model_type}/{domain}/{model_name}/cross_{fold}')
 
@@ -74,17 +94,9 @@ def compute_metrics(pred):
     accuracy = accuracy_metric.compute(predictions=pred_ids, references=pred.label_ids)
     return accuracy
 
-model = Wav2Vec2LoanwordsModel.from_pretrained(
-    f'../{base_model}',
-    id2label = {i: v for i, v in enumerate(dataset['train_0'].features['label'].names)},
-    label2id = {v: i for i, v in enumerate(dataset['train_0'].features['label'].names)},
-    use_weighted_layer_sum = True,
-    classifier_head = True,
-    classifier_hidden = True,
-    classifier_hidden_activation_function = 'relu',
-    **model_config,
-)
-model.freeze_base_model()
+model = model_init_fn(**model_config)
+if isinstance(model, Wav2Vec2LoanwordsModel):
+    model.freeze_base_model()
 
 train_dataset = dataset[f'train_{fold}']
 test_dataset = dataset[f'dev_{fold}']
@@ -100,7 +112,7 @@ training_args = TrainingArguments(
     weight_decay=0.005,
     warmup_steps=0.25,
     push_to_hub=False,
-    output_dir=os.path.expanduser(f'~/scratch/trainer_output_{model_name}_class_{base_dataset}{dataset_var}_varHiddenRelu_cross_{fold}'),
+    output_dir=os.path.expanduser(f'~/scratch/trainer_output_{base_model}_{model_name}_class_{base_dataset}{dataset_var}_varHiddenRelu_cross_{fold}'),
     report_to='tensorboard',
     train_sampling_strategy='group_by_length',
     metric_for_best_model='eval_loss',
